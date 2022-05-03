668b128ac21a43cecb575e8228f9460ca08c12cc
bensouthgate <bensouthgate@gmail.com>
2022-05-03 10:27:58 +0100

    BW-5 DMR rule

--- workflow/scripts/dmrcatedmr.R
+++ workflow/scripts/dmrcatedmr.R
@@ -3,6 +3,19 @@
 # Email: james.ashmore@zifornd.com ben.southgate@zifornd.com
 # License: MIT
 
+#### Liftover
+liftover <- function(results.ranges, chainfile){
+
+  # hg19ToHg38.over.chain from https://hgdownload.soe.ucsc.edu/goldenPath/hg19/liftOver/
+
+  ch <- import.chain(chainfile)
+
+  results <- rtracklayer::liftOver(results.ranges, ch)
+
+  results <- unlist(results)
+  
+  return(results)
+}
 
 main <- function(input, output, params, log, config) {
   
@@ -21,33 +34,40 @@ main <- function(input, output, params, log, config) {
   library(DMRcate)
   library(rtracklayer)
   library(minfi)
+  library(ExperimentHub)
   
   cpg.annotation <- readRDS(input$rds)
   
+  # Below are default values
+  # fdr cutoff default applied and advised by authors
+  # see opt pcutoff = "fdr"
   dmrcoutput <- dmrcate(cpg.annotation, lambda=1000, C=2)
-  
-  results.ranges <- extractRanges(dmrcoutput, genome = "hg19")
-  
-  #### Liftover
-  # hg19ToHg38.over.chain from https://hgdownload.soe.ucsc.edu/goldenPath/hg19/liftOver/
-  
-  path <- system.file(package="liftOver", "extdata", "hg19ToHg38.over.chain")
-  
-  ch <- import.chain(path)
-  
-  seqlevelsStyle(results.ranges) = "UCSC"  # necessary
-  
-  cur38 <- liftOver(results.ranges, ch)
-  class(cur38)
-  cur38 <- unlist(cur38)
-  
-  #### dmrcate output 
-  results = cur38
-  
-  # order by fdr
-  results = results[order(results$minfdr),]
 
+  results.ranges <- extractRanges(dmrcoutput, genome = "hg19")
+  #results.ranges <- dmrcateExtractRanges(dmrcoutput, genome = "hg19")
+
+  seqlevelsStyle(results.ranges) <- "UCSC"
+
+  chainfile <- params$chainfile
+
+  if(params$liftover){
+
+    results <- liftover(results.ranges, chainfile)
+
+  } else {
+
+    results <- results.ranges
+
+  }
   
+  # order by fdr of choice
+
+  fdr <- params$fdr
+
+  results = results[order(mcols(results)[[fdr]]),]
+
+  write.csv(as.data.frame(results), file = output$csv, quote = F)
+
   saveRDS(results, file = output$rds)
   
 }
